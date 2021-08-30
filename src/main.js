3168dbe8d450ce3166439c2c784014403397dc9d
NhatTanUT <tvnt221@gmail.com>
2021-08-30 17:41:20 +0700

    2

--- src/main.js
+++ src/main.js
@@ -9,8 +9,6 @@ import axios from 'axios'
 axios.defaults.headers.common['Content-Type'] = 'application/x-www-form-urlencoded'
 axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
 
-Vue.http.options.credentials = true;
-
 Vue.http.options.xhr = {
   withCredentials: true
 }
