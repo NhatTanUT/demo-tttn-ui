3ccdb634b3ae449a3cb712dc95eeece51aed0453
NhatTanUT <tvnt221@gmail.com>
2021-08-30 17:56:59 +0700

    4

--- src/main.js
+++ src/main.js
@@ -5,18 +5,21 @@ import router from './router'
 import vuetify from './plugins/vuetify'
 import {Tabs, Tab} from 'vue-tabs-component';
 import axios from 'axios'
+import VueResource from 'vue-resource'
+
+Vue.use(VueResource)
 
 axios.defaults.headers.common['Content-Type'] = 'application/x-www-form-urlencoded'
 axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
 
-// Vue.http.options.credentials = true;
+Vue.http.options.credentials = true;
 
-// Vue.http.options.xhr = {
-//   withCredentials: true
-// }
-// Vue.http.options.emulateJSON = true
-// Vue.http.options.emulateHTTP = true
-// Vue.http.options.crossOrigin = true
+Vue.http.options.xhr = {
+  withCredentials: true
+}
+Vue.http.options.emulateJSON = true
+Vue.http.options.emulateHTTP = true
+Vue.http.options.crossOrigin = true
 
 Vue.http.headers.common['Access-Control-Allow-Origin'] = '*'
 Vue.http.headers.common['Content-Type'] = 'application/x-www-form-urlencoded'
